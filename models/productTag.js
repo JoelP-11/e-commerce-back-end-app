7de8fd2a85f1ad90b0079b81cccca1f5be5386b3
JoelP-11 <Jp421513@gmail.com>
2023-07-21 01:35:55 -0700

    rearranged the files and got rid of the develop folder, also finished product.js

--- models/productTag.js
+++ models/productTag.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../develop/config/connection');
+const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
--- models/tag.js
+++ models/tag.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../develop/config/connection.js.js');
+const sequelize = require('../config/connection.js.js.js');
 
 class Tag extends Model {}
 
